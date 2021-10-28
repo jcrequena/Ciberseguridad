dd5b6312d70846decbc0ff4619651c174c262dcf
Juan Carlos <jcrequena.fp@gmail.com>
2021-10-28 19:22:33 +0200

    Update Crear-usuarioLocal.ps1

--- BRS/Proyectos/RedCentralizada/Crear-usuarioLocal.ps1
+++ BRS/Proyectos/RedCentralizada/Crear-usuarioLocal.ps1
@@ -4,7 +4,7 @@ $passAccount=ConvertTo-SecureString $pass -AsPlainText -force
 
 New-LocalUser “nombre_usuario” -Password $passAccount –Fullname “Nombre completo” -Description “Descripción de la cuenta”
 
-
+#Añadir un usuario a un grupo
 Add-LocalGroupMember -Group “Administradores” -Member “usuario_creado” 
 
 
