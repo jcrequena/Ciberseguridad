efc18e0e3f7dec489afec0b8b93d7202b08d30b7
Juan Carlos <jcrequena.fp@gmail.com>
2021-11-25 16:42:26 +0100

    Update serverRadius.ps1

--- BRS/Proyectos/AAA/serverRadius.ps1
+++ BRS/Proyectos/AAA/serverRadius.ps1
@@ -1,17 +1,18 @@
 #
-#1. Instalar el Rol Servicios de acceso y directivas de redes
+# 1. Instalar el Rol Servicios de acceso y directivas de redes
 #
-Install-WindowsFeature NPAS -IncludeManagementTools
+Install-WindowsFeature -name NPAS -IncludeAllSubFeature  -IncludeManagementTools
 
-#
-#Una vez se instala el cliente Radius
-#
+# 2. Configuración del servidor RADIUS
+# Crear un cliente radius
+# https://docs.microsoft.com/en-us/powershell/module/nps/new-npsradiusclient?view=windowsserver2019-ps
+# Este comando, configurara un cliente Radius,donde
+# Name, será el nombre que le daremos a la maquina que usaremos como cliente.
+# Address será la Ip de dicha maquina.
+# VendorName será el tipo de radius que usaremos.
+# Sharesecret la contraseña para establecer la conexión.
 
-#2. Configuración del servidor RADIUS
-#Crear un cliente radius
-PS C:\>New-NpsRadiusClient -Name “ClienteRADIUS” -Address “192.168.11.253” `
--SharedSecret “eeepc20” `
--VendorName “Radius Standard” Restart-Service IAS
+New-NpsRadiusClient -Name “ClientRadius-Subdominio1” -Address “192.168.11.253” `-SharedSecret “eeepc20” `
 
-#Reiniciamos el servicio
-PS C:\>Get-NPSSharedSecretTemplate
+# Reiniciamos el servicio para que actualice las nueos clientes radius
+Restart-Service IAS
