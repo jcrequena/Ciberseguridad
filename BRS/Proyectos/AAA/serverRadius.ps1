31195f268bca1ea4eba5eee793db0ba437176467
Juan Carlos <jcrequena.fp@gmail.com>
2021-11-25 17:03:14 +0100

    Update serverRadius.ps1

--- BRS/Proyectos/AAA/serverRadius.ps1
+++ BRS/Proyectos/AAA/serverRadius.ps1
@@ -4,7 +4,13 @@
 Install-WindowsFeature -name NPAS -IncludeAllSubFeature  -IncludeManagementTools
 
 # 2. Configuración del servidor RADIUS
-# Crear un cliente radius
+# 2.1 Registrar el servidor NPS en Active Directory. 
+#     De esta forma, el servidor tendrá autoridad para leer las propiedades de las cuentas de usuario de Active Directory para autenticar a los usuarios. 
+#     El servidor se agregará al grupo de dominio integrado Servidores RAS e IAS.
+netsh ras add registeredserver
+
+
+# 2.2 Añadir clientes radius
 # https://docs.microsoft.com/en-us/powershell/module/nps/new-npsradiusclient?view=windowsserver2019-ps
 # Este comando, configurara un cliente Radius,donde
 # Name, será el nombre que le daremos a la maquina que usaremos como cliente.
@@ -16,3 +22,11 @@ New-NpsRadiusClient -Name “ClientRadius-Subdominio1” -Address “192.168.11.
 
 # Reiniciamos el servicio para que actualice las nueos clientes radius
 Restart-Service IAS
+
+# 2.3 Configurar NPS Policies
+#     Las políticas de NPS permiten autenticar usuarios remotos y otorgarles permisos de acceso configurados en el rol de NPS.
+#     1. Políticas de solitud de conexión
+#     2. Políticas de red.
+#     Se pueden crear en un servidor con GUI y exportarlas para luego importarlas en el Servidor Radius Core.     
+#     Export-NpsConfiguration -Path c:\ps\backup_nps.xml
+#     Import-NpsConfiguration -Path c:\ps\backup_nps.xml
