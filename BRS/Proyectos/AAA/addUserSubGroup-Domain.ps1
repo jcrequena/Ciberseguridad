a6b0d1c79a93a909f360badaccbb4c1c81ff329a
Juan Carlos <jcrequena.fp@gmail.com>
2021-11-25 15:55:36 +0100

    Create addUserSubGroup-Domain.ps1

---
+++ BRS/Proyectos/AAA/addUserSubGroup-Domain.ps1
@@ -0,0 +1,11 @@
+#Añadir un usuario de un subdominio a un grupo del dominio raiz
+#
+Add-ADGroupMember -Identity nombreGrupoGlobalSeguridad -Members subdomino\NombreUsuario
+
+#Caso de ejemplo
+#dominio: ciber.local
+#subdominio:brs.ciber.local
+#Grupo Global dominio raíz: CIBER-GG-Radius
+#Usuario del subdominio brs.ciber.local: jcrequena
+#El comando a ejcutar en el domino raíz ciber.local sería:
+Add-ADGroupMember -Identity  CIBER-GG-Radius -Members brs.ciber.local\jcrequena
