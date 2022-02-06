092d0bd042f54aa471771bcd167981066016d7b6
Juan Carlos <jcrequena.fp@gmail.com>
2022-02-06 12:38:04 +0100

    Update OpenvSwitch.sh

--- BRS/VLAN/OpenvSwitch.sh
+++ BRS/VLAN/OpenvSwitch.sh
@@ -1,5 +1,5 @@
 sudo apt update -y && apt upgrade -y
-sudo apt install unbound libtool
+sudo apt install libtool
 git clone https://github.com/openvswitch/ovs.git
 cd ovs/
 sudo ./boot.sh
@@ -9,16 +9,36 @@ sudo apt install make
 sudo /sbin/modprobe openvswitch
 sudo /sbin/lsmod | grep openvswitch
 export PATH=$PATH:/usr/local/share/openvswitch/scripts
+
+sudo snap install openvswitch --edge
+
+
 sudo ovs-ctl start
 ovs-vsctl show
 
-ovs-vsctl add-br ciber-bridge
-ovs-vsctl add-port ciber-bridge enp0s8
-ovs-vsctl add-port ciber-bridge enp0s9
+ovs-vsctl add-br lan0
 
-ovs-vsctl set-port enp0s8 tag=10
-ovs-vsctl set-port enp0s8 tag=10
-ovs-vsctl set-port enp0s9 tag=10
+for tap in `seq 0 7`; do
+  ip tuntap add mode tap lan0p$tap
+done;
+ip tuntap list
+
+for tap in `seq 0 7`; do 
+        ip link set lan0p$tap up
+done;
+
+for tap in `seq 0 7`; do
+        ovs-vsctl add-port lan0 lan0p$tap
+done;
+ovs-vsctl list-ports lan0
+
+
+#ovs-vsctl add-port ciber-bridge enp0s8
+#ovs-vsctl add-port ciber-bridge enp0s9
+
+#ovs-vsctl set-port enp0s8 tag=10
+#ovs-vsctl set-port enp0s8 tag=10
+#ovs-vsctl set-port enp0s9 tag=10
 ovs-appctl fdb/show ciber-bridge
 
 #Borrar puertos
