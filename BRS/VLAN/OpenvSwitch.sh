9f5db23ddc1855705cebdb664d5a5996ace974d3
Juan Carlos <jcrequena.fp@gmail.com>
2022-02-06 17:10:44 +0100

    Update OpenvSwitch.sh

--- BRS/VLAN/OpenvSwitch.sh
+++ BRS/VLAN/OpenvSwitch.sh
@@ -1,15 +1,3 @@
-sudo apt update -y && apt upgrade -y
-sudo apt install libtool
-git clone https://github.com/openvswitch/ovs.git
-cd ovs/
-sudo ./boot.sh
-sudo ./configure
-sudo make
-sudo apt install make
-sudo /sbin/modprobe openvswitch
-sudo /sbin/lsmod | grep openvswitch
-export PATH=$PATH:/usr/local/share/openvswitch/scripts
-#######################################################3
 sudo -s
 apt-get update -y
 apt-get install -y openvswitch-switch
@@ -34,15 +22,12 @@ done;
 ovs-vsctl list-ports lan0
 
 
-#ovs-vsctl add-port ciber-bridge enp0s8
-#ovs-vsctl add-port ciber-bridge enp0s9
+#Añadir etiquetas VLAN
 
-#ovs-vsctl set-port enp0s8 tag=10
-#ovs-vsctl set-port enp0s8 tag=10
-#ovs-vsctl set-port enp0s9 tag=10
-ovs-appctl fdb/show ciber-bridge
+ovs-vsctl set-port lan0p0 tag=10
+ovs-vsctl set-port lan0p1 tag=20
+
 
 #Borrar puertos
-ovs-vsctl del-port ciber-bridge enp0s8
-ovs-vsctl del-port ciber-bridge enp0s9
+ovs-vsctl del-port lan0 lan0p0
 
