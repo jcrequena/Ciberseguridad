c19296fe1d09ac3d75c870ade3611435061f6693
Juan Carlos <jcrequena.fp@gmail.com>
2022-02-10 21:54:51 +0100

    Update crearCertificado.ps1

--- BRS/RadiusWServer/crearCertificado.ps1
+++ BRS/RadiusWServer/crearCertificado.ps1
@@ -1,2 +1,6 @@
-$date=
-
+$date = (Get-Date).ToString('MMM-yyyy')
+$certificateExpiringYears = (Get-Date).AddYears(2)
+$FQND = "wificontroller.ciber.local"
+$friendlyname = "wificontroller"
+New-SelfSignedCertificate -DnsName $FQND -KeyLength 2048 -CertStoreLocation "Cert:LocalMachine\My" `
+-FriendlyName $friendlyname -NotAfter $certificateExpiringYears
