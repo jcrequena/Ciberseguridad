5c3bdd014272aff6293ca2d7ca048927f9a14356
Juan Carlos <jcrequena.fp@gmail.com>
2022-02-11 17:00:37 +0100

    Update freeRadiusLDAP.sh

--- BRS/RADIUS/freeRadiusLDAP.sh
+++ BRS/RADIUS/freeRadiusLDAP.sh
@@ -1,6 +1,7 @@
 #https://sysopstechnix.com/wpa2-enterprise-secure-your-organization-wi-fi-network/
 hostnamectl set-hostname radius-openldap.ciber.local
 nano /etc/hosts
+127.0.1.1 radius-openldap
 192.168.0.30 radius-openldap.ciber.local
 
 apt update -y && apt upgrade -y
@@ -9,44 +10,75 @@ apt install slapd ldap-utils
 root@radius-openldap:~# slappasswd
 New password:
 Re-enter new password:
+
 nano create_ldap_objects.ldif
-#Create organizational Units
+#Crear UOs
 dn: ou=depInformatica,dc=ciber,dc=local
 objectClass: organizationalUnit
 ou: depInformatica
+
 dn: ou=grupos,ou=depInformatica,dc=ciber,dc=local
 objectClass: organizationalUnit
 ou: grupos
 #Create LDAP Users
-dn: uid=usuInf-01,ou=depInformatica,dc=ciber,dc=local
+dn: uid=user01,ou=depInformatica,dc=ciber,dc=local
 objectClass: inetOrgPerson
 objectClass: organizationalPerson
 objectClass: top
 cn: Usuario 01 Informática
-sn: usuInf01
+sn: user01
 displayName: Usuario 01 Informática
-givenName: UsuInf01
-mail: usuInf-01@ciber.local
+givenName: Informática
+mail: user01@ciber.local
 userPassword: {SSHA}jFQAulicFL0des89xJCDVkyrLlq3dAT3
-dn: uid=usuInf-02,ou=depInformatica,dc=ciber,dc=local
+
+dn: uid=user02,ou=depInformatica,dc=ciber,dc=local
 objectClass: inetOrgPerson
 objectClass: organizationalPerson
 objectClass: top
 cn: Usuario 02 Informática
-sn: usuInf02
+sn: user02
 displayName: Usuario 02 Informática
-givenName: UsuInf02
-mail: usuInf-02@ciber.local
+givenName: Informática
+mail: user01@ciber.local
+userPassword: {SSHA}jFQAulicFL0des89xJCDVkyrLlq3dAT3
+
+dn: uid=user03,ou=depInformatica,dc=ciber,dc=local
+objectClass: inetOrgPerson
+objectClass: organizationalPerson
+objectClass: top
+cn: Usuario 03 Informática
+sn: user03
+displayName: Informática User03
+givenName: Informática
+mail: user03@ciber.local
+userPassword: {SSHA}jFQAulicFL0des89xJCDVkyrLlq3dAT3
+
+dn: uid=user04,ou=depInformatica,dc=ciber,dc=local
+objectClass: inetOrgPerson
+objectClass: organizationalPerson
+objectClass: top
+cn: Informatica User04
+sn: user04
+displayName: Usuarios 04 Informática
+givenName: Informática
+mail: user04@ciber.local
 userPassword: {SSHA}jFQAulicFL0des89xJCDVkyrLlq3dAT3
 
 #Create global group
-dn: cn=GGDepInformatica,ou=grupos,ou=depInformatica,dc=ciber,dc=local
+dn: cn=GGProgramacion,ou=grupos,ou=depInformatica,dc=ciber,dc=local
 objectClass: groupOfNames
 objectClass: top
-cn: GGDepInformatica
-#Add users in global group
-member: uid=usuInf-01,ou=depInformatica,dc=ciber,dc=local
-member: uid=usuInf-02,ou=depInformatica,dc=ciber,dc=local
+cn: GGProgramacion
+member: uid=user01,ou=depInformatica,dc=ciber,dc=local
+member: uid=user02,ou=depInformatica,dc=ciber,dc=local
+
+dn: cn=GGRedes,ou=grupos,ou=depInformatica,dc=ciber,dc=local
+objectClass: groupOfNames
+objectClass: top
+cn: GGRedes
+member: uid=user03,ou=depInformatica,dc=ciber,dc=local
+member: uid=user04,ou=depInformatica,dc=ciber,dc=local
 
 #Añadir el fichero al servidor LDAP
 ldapadd -x -D cn=admin,dc=ciber,dc=local -W -f create_ldap_objects.ldif 
