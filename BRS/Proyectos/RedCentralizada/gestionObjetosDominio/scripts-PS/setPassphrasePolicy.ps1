28c82c10df253b3e2dc26d6cdadd70ef160ce28a
Juan Carlos <jcrequena.fp@gmail.com>
2022-11-02 15:23:00 +0100

    Update setPassphrasePolicy.ps1

--- BRS/Proyectos/RedCentralizada/gestionObjetosDominio/scripts-PS/setPassphrasePolicy.ps1
+++ BRS/Proyectos/RedCentralizada/gestionObjetosDominio/scripts-PS/setPassphrasePolicy.ps1
@@ -5,4 +5,14 @@
 #Requiere una contraseña
 #fuente: https://learn.microsoft.com/es-es/powershell/module/configurationmanager/new-cmfdvpassphrasepolicy?view=sccm-ps
 
-New-CMFDVPassPhrasePolicy -PolicyState Enabled -PasswordComplexity Require -MinimumLength 12 -RequirePassword
+New-CMFDVPassPhrasePolicy -PolicyState Enabled -PasswordComplexity Require -MinimumLength 4 -RequirePassword
+
+#Bloqueo de cuentas 
+#Se configura para que se bloquee si se falla al intento 3.
+net accounts /lockoutthreshold:3
+#Duración del bloqueo 60 minutos
+net accounts /lockoutduration:60
+#Ventana de bloqueo (tiempo en minutos antes de que se restablezca el contador de intentos fallidos de contraseña )
+net accounts /lockoutwindow:60
+#Para consultar la configuración de los parámetros establecidos
+net accounts 
