bb6c6e7a5226c791b585236d324e243399669117
Juan Carlos <jcrequena.fp@gmail.com>
2022-11-02 19:35:02 +0100

    Create AD_configura.ps1

---
+++ BRS/Proyectos/RedCentralizada/AD_configura.ps1
@@ -0,0 +1,5 @@
+$dominioFQDN = "ciber.local"
+$dominioNETBIOS = "ciber"
+Install-WindowsFeature AD-Domain-Services,DNS
+Import-module addsdeployment
+Install-ADDSForest -DomainName $dominioFQDN -DomainNetBiosName $dominioNETBIOS -SafeModeAdministratorPassword (ConvertTo-SecureString -string "P@ssw0rd" -AsPlainText -Force) -DomainMode WinThreshold -ForestMode WinThreshold -InstallDNS -Confirm:$false
