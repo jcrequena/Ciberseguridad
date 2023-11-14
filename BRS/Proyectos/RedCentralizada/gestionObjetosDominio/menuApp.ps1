7a7607ef233ca8182ee16238705eb86c6806c149
Juan Carlos <jcrequena.fp@gmail.com>
2023-11-14 18:56:42 +0100

    Update menuApp.ps1

--- BRS/Proyectos/RedCentralizada/gestionObjetosDominio/menuApp.ps1
+++ BRS/Proyectos/RedCentralizada/gestionObjetosDominio/menuApp.ps1
@@ -1,78 +1,119 @@
-# Fuente: https://gallery.technet.microsoft.com/scriptcenter/Menu-simple-en-PowerShell-95e1f923
-# Paso de parámetros al ejecutar el script en la consola
-# Este script recoge 2 parámetros en la llamada del mismo en la consola. 
-# La ejecución del script será de esta forma, donde el parámetro distinguishedName será el de vuestro subdominio.
-# ejemplo: brs.ciber.local, donde brs es el subdominio de ciber.local
-#./menuApp.ps1 distinguishedName
-Param(
-  [string]$Param1,
-)
-$distinguishedName= $Param1
+#Variables globales
+$domain="dc=ciber,dc=mylocal"
+#
+#Funciones en la cabecera del script
+#
 
-
-#Función 1. Crear grupos
-function crearGrupos
+function Show-Menu
 {
-    Write-Host "Crear grupos.."
+     param (
+           [string]$Titulo = 'Menú principal'
+     )
+     Clear-Host
+     Write-Host "================ $Titulo ================"
+    
+     Write-Host "1: Opción '1' Crear UOs."
+     Write-Host "2: Opción '2' Crear Grupos."
+     Write-Host "3: Opción '3' Crear Usuarios."
+     Write-Host "Q: Opción 'Q' Salir."
 }
-
-#Función 1. Crear administradoresParciales
-function crearAdminParciales
+function alta_UOs
 {
-  Write-Host "Crear AdminParciales.."
+     $ficheroCsvUO=Read-Host "Introduce el fichero csv de UO's:"
+     $ficheroImportado = import-csv -Path $ficheroCsvUO -delimiter :
+     foreach($line in $ficheroImportado)
+     {
+          New-ADOrganizationalUnit -Description:$line.Description -Name:$line.Name `
+		-Path:$line.Path -ProtectedFromAccidentalDeletion:$true 
+     }
+     Write-Host "Se han creado las UOs satisfactoriamente en el dominio $domain"
 }
-
-#Función mostrarMenu muestra un menú por pantalla con 3 opciones y una última para salir del mismo
-# La función “mostrarMenu”, puede tomar como parámetro un título y devolverá por pantalla 
-# "================ $Titulo================" , donde $Titulo será el título pasado por parámetro.
-#Si no se le pasa un parámetro, por defecto $Titulo contendrá la cadena 'Selección de opciones'  
-#https://technet.microsoft.com/es-es/library/jj554301.aspx
-function mostrarMenu 
-{ 
-     param ( 
-           [string]$Titulo = 'Selección de opciones' 
-     ) 
-     Clear-Host 
-     Write-Host "================ $Titulo================" 
-      
+function alta_grupos
+{
+     	$gruposCsv=Read-Host "Introduce el fichero csv de Grupos:"
+	$ficheroImportado = import-csv -Path $gruposCsv -delimiter :
+	foreach($linea in $ficheroImportado)
+	{
+		
+		New-ADGroup -Name:$linea.Name -Description:$linea.Description `
+			-GroupCategory:$linea.Category `
+			-GroupScope:$linea.Scope  `
+			-Path:$linea.Path
+	}
+ 	 Write-Host "Se han creado las grupos satisfactoriamente en el dominio $domain"
      
-     Write-Host "1. Primera Opción" 
-     Write-Host "2. Segunda Opción" 
-     Write-Host "3. Tercera Opción" 
-     Write-Host "S. Presiona 'S' para salir" 
 }
-#Bucle principal del Script. El bucle se ejecuta de manera infinita hasta que se cumple
-#la condición until ($input -eq 's'), es decir, hasta que se pulse la tecla s.
-do 
-{ 
-     #Llamamos a la función mostrarMenu, para dibujar el menú de opciones por pantalla
-     mostrarMenu 
-     #Recogemos en la varaible input, el valor que el usuario escribe por teclado (opción del menú)
-     $input = Read-Host "Elige una opción" 
-     switch ($input) 
-     { 
-           '1' { 
-                Clear-Host  
-                'Primera Opción' 
-                pause
-           } '2' { 
-                Clear-Host  
-                'Segunda Opción' 
-                pause
-           } '3' { 
-                Clear-Host  
-                'Tercera Opción' 
-                pause
-           } 's' {
-                'Saliendo del script...'
-                return 
-           } 
-           #Si no se selecciona una de las opciones del menú, es decir, se pulsa algun carácter
-           #que no sea 1, 2, 3 o s, sacamos por pantalla un aviso e indicamos lo que hay que realizar.
-           default { 
-              'Por favor, Pulse una de las opciones disponibles [1-3] o s para salir'
+function alta_usuarios
+{
+	$fileUsersCsv=Read-Host "Introduce el fichero csv de los usuarios:"
+	$ficheroImportado = import-csv -Path $fileUsersCsv -Delimiter : 				     
+	foreach($linea in $ficheroImportado)
+	{
+		$passAccount=ConvertTo-SecureString $linea.DNI -AsPlainText -force
+		$Surnames=$linea.FirstName+' '+$linea.LastName
+		$nameLarge=$linea.Name+' '+$linea.FirstName+' '+$linea.LastName
+		$email=$linea.Email
+		[boolean]$Habilitado=$true
+    		If($linea.Enabled -Match 'false') { $Habilitado=$false}
+		#Establecer los días de expiración de la cuenta (Columna del csv ExpirationAccount)
+   		$ExpirationAccount = $linea.ExpirationAccount
+    		$timeExp = (get-date).AddDays($ExpirationAccount)
+		#
+		# Ejecutamos el comando para crear el usuario
+		#
+		New-ADUser -SamAccountName $linea.Account -UserPrincipalName $linea.Account -Name $linea.Account `
+			-Surname $Surnames -DisplayName $nameLarge -GivenName $linea.Name `
+			-Description "Cuenta de $nameLarge" -EmailAddress $email `
+			-AccountPassword $passAccount -Enabled $Habilitado `
+			-CannotChangePassword $false -ChangePasswordAtLogon $true `
+			-PasswordNotRequired $false -Path $linea.Path -AccountExpirationDate $timeExp `
+   		-LogonWorkstations $linea.computer
+		
+  		#
+  		## Establecer horario de inicio de sesión       
+      $horassesion = $linea.NetTime -replace(" ","")
+      net user $linea.Account /times:$horassesion 
+		
+  		#Asignar cuenta de Usuario a Grupo
+		# Distingued Name CN=Nombre-grupo,ou=..,ou=..,dc=..,dc=...
+		#En este caso el grupo se encuentra en la misma UO que el usuario
+    $cnGrpAccount="Cn="+$linea.Group+","+$linea.Path
+		Add-ADGroupMember -Identity $cnGrpAccount -Members $linea.Account
+		
+	}     
+}
+
+
+#Primero comprobaremos si se tiene cargado el módulo Active Directory
+if (!(Get-Module -Name ActiveDirectory)) #Accederá al then solo si no existe una entrada llamada ActiveDirectory
+{
+  Import-Module ActiveDirectory #Se carga el módulo
+}
+
+
+#
+#MENU PRINCIPAL
+#
+do
+{
+     Show-Menu
+     $input = Read-Host "Por favor, pulse una opción"
+     switch ($input)
+     {
+           '1' {
+                Clear-Host
+                alta_UOs
+           } '2' {
+                Clear-Host
+                alta_grupos
+           } '3' {
+                Clear-Host
+                alta_usuarios
+           } 'q' {
+                'Salimos de la App'
+                return
            }
-     } 
-     pause 
-} 
-until ($input -eq 's')
+     }
+     pause
+}
+until ($input -eq 'q')
