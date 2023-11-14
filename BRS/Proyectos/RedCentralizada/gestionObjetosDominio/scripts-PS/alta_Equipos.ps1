1be3ca0f4f5f17dde417dc970a46fee66983370c
Juan Carlos <jcrequena.fp@gmail.com>
2023-11-14 19:00:10 +0100

    Update alta_Equipos.ps1

--- BRS/Proyectos/RedCentralizada/gestionObjetosDominio/scripts-PS/alta_Equipos.ps1
+++ BRS/Proyectos/RedCentralizada/gestionObjetosDominio/scripts-PS/alta_Equipos.ps1
@@ -1,4 +1,3 @@
-# alta_Equipos.ps1 : Parámetro 1 el dc (nombre netbios del dominio) parámetro 2 el sufijo del dominio 
 #Referencia: https://technet.microsoft.com/es-es/library/hh852258(v=wps.630).aspx
 #Para este caso de ejemplo, usamos el csv que contiene lo siguiente
 #Computer:Path
@@ -8,12 +7,8 @@
 #Por lo tanto para crear el equipo en esa ruta sobre el dominio smr.local quedaría así:
 #OU=Equipos-DepInf,OU=Dep-Informatica,DC=smr,DC=local
 
-param($dominio,$sufijoDominio)
-
-#Componemos el Domain Component para el dominio que se pasa por parámetro
-# en este caso, el dominio es smr.local
-#Por lo que hay que componer dc=smr,dc=local
-$domainComponent="dc="+$dominio+",dc="+$sufijoDominio
+#Ponemos el Domain Component para el dominio en cuestión, que para este caso es smr.local
+$domain="dc=smr,dc=local"
 
 #
 #Creación de los grupos a partir de un fichero csv
@@ -24,15 +19,9 @@ $fichero= import-csv -Path $equiposCsv -delimiter ":"
 
 foreach($line in $fichero)
 {
-	$pathObject=$line.Path+","+$domainComponent	
-	#Comprobamos que no exista el equipo en el sistema
-	if ( !(Get-ADComputer -Filter { name -eq $line.Computer }) )
-	{
-		New-ADComputer -Enabled:$true -Name:$line.Computer -Path:$pathObject -SamAccountName:$line.Computer
-	}
-	else { Write-Host "El equipo $line.Computer ya existe en el sistema"}
+	New-ADComputer -Enabled:$true -Name:$line.Computer -Path:$line.Path -SamAccountName:$line.Computer
 }
 
 write-Host ""
-write-Host "Se han creado los equipos" -Fore green
+write-Host "Se han creado los equipos en el dominio $domain" -Fore green
 write-Host "" 
